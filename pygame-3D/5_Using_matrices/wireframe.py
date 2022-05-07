719b8dbf4372a2c678be7ccfeccf98cdac4a42c2
czechoa <andrzej.czechu.ja@gmail.com>
2022-05-07 13:01:49 +0200

    display only nodes  with transforms

--- pygame-3D/5_Using_matrices/wireframe.py
+++ pygame-3D/5_Using_matrices/wireframe.py
@@ -1,46 +1,118 @@
 import numpy as np
 
+
 class Wireframe:
     def __init__(self):
         self.nodes = np.zeros((0, 4))
+        self.nodes_2d_throw = np.zeros((0, 2))
         self.edges = []
+
 
     def addNodes(self, node_array):
         ones_column = np.ones((len(node_array), 1))
         ones_added = np.hstack((node_array, ones_column))
         self.nodes = np.vstack((self.nodes, ones_added))
+        self.nodes_2d_throw = self.nodes[:, :2]
 
     def addEdges(self, edgeList):
         self.edges += edgeList
 
     def outputNodes(self):
-        for i in range(self.nodes.shape[1]):
-            (x, y, z, _) = self.nodes[:, i]
-            print "Node %d: (%.3f, %.3f, %.3f)" % (i, x, y, z)
+
+        for i in range(self.nodes.shape[0]):
+            x, y, z,_ = self.nodes[i]
+            print("Node %d: (%.3f, %.3f, %.3f)" % (i, x, y, z))
             
     def outputEdges(self):
         for i, (start, stop) in enumerate(self.edges):
-            node1 = self.nodes[:, start]
-            node2 = self.nodes[:, stop]
-            print "Edge %d: (%.3f, %.3f, %.3f)" % (i, node1[0], node1[1], node1[2]),
-            print "to (%.3f, %.3f, %.3f)" % (node2[0], node2[1], node2[2])
+            node1 = self.nodes[start]
+            node2 = self.nodes[stop]
+            print( "Edge %d: (%.3f, %.3f, %.3f)" % (i, node1[0], node1[1], node1[2]))
+            print( "to (%.3f, %.3f, %.3f)" % (node2[0], node2[1], node2[2]))
 
     def findCentre(self):
-        num_nodes = len(self.nodes)
-        meanX = sum([node.x for node in self.nodes]) / num_nodes
-        meanY = sum([node.y for node in self.nodes]) / num_nodes
-        meanZ = sum([node.z for node in self.nodes]) / num_nodes
-        
+        meanX, meanY, meanZ, _ = self.nodes.mean(axis=0)
         return (meanX, meanY, meanZ)
+
+
+
+    def transform(self, matrix):
+        """ Apply a transformation defined by a given matrix. """
+        self.nodes = np.dot(self.nodes, matrix)
+
+    def scale(self,sx=0, sy=0, sz=0):
+        """ Return matrix for scaling equally along all axes centred on the point (cx,cy,cz). """
+
+        matrix = scaleMatrix(sx,sy,sz)
+
+        self.transform(matrix)
+
+    def rotateX(self,theta=1):
+        self.transform(rotateXMatrix(theta))
+
+    def rotateY(self,theta=1):
+        self.transform(rotateYMatrix(theta))
+
+    def rotateZ(self,theta=1):
+        self.transform(rotateZMatrix(theta))
 
 if __name__ == "__main__":
     cube = Wireframe()
     cube_nodes = [(x,y,z) for x in (0,1) for y in (0,1) for z in (0,1)]
     cube.addNodes(np.array(cube_nodes))
-    
+
     cube.addEdges([(n,n+4) for n in range(0,4)])
     cube.addEdges([(n,n+1) for n in range(0,8,2)])
     cube.addEdges([(n,n+2) for n in (0,1,4,5)])
-    
+
+    cube.findCentre()
     cube.outputNodes()
     cube.outputEdges()
+
+
+def translationMatrix(dx=0, dy=0, dz=0):
+    """ Return matrix for translation along vector (dx, dy, dz). """
+
+    return np.array([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, 0],
+                     [dx, dy, dz, 1]])
+
+def rotateXMatrix(radians):
+    """ Return matrix for rotating about the x-axis by 'radians' radians """
+
+    c = np.cos(radians)
+    s = np.sin(radians)
+    return np.array([[1, 0, 0, 0],
+                     [0, c, -s, 0],
+                     [0, s, c, 0],
+                     [0, 0, 0, 1]])
+
+def rotateYMatrix(radians):
+    """ Return matrix for rotating about the y-axis by 'radians' radians """
+
+    c = np.cos(radians)
+    s = np.sin(radians)
+    return np.array([[c, 0, s, 0],
+                     [0, 1, 0, 0],
+                     [-s, 0, c, 0],
+                     [0, 0, 0, 1]])
+
+def rotateZMatrix(radians):
+    """ Return matrix for rotating about the z-axis by 'radians' radians """
+
+    c = np.cos(radians)
+    s = np.sin(radians)
+    return np.array([[c, -s, 0, 0],
+                     [s, c, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
+
+def scaleMatrix(sx=0, sy=0, sz=0):
+    """ Return matrix for scaling equally along all axes centred on the point (cx,cy,cz). """
+
+    return np.array([[sx, 0, 0, 0],
+                     [0, sy, 0, 0],
+                     [0, 0, sz, 0],
+                     [0, 0, 0, 1]])
+
