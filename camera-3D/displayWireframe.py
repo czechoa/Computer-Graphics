ea0297dc9223456b5bd313033c6ce6325eadb52b
czechoa <andrzej.czechu.ja@gmail.com>
2022-05-16 11:22:48 +0200

    make painter algorrytm,

--- camera-3D/displayWireframe.py
+++ camera-3D/displayWireframe.py
@@ -19,7 +19,7 @@ key_to_function = {
     pygame.K_l: (lambda x: x.translateAll([0, 0, -10])),
 
     pygame.K_EQUALS: (lambda x: x.scaleAll(1.5)),
-    pygame.K_MINUS: (lambda x: x.scaleAll(1/1.5)),
+    pygame.K_MINUS: (lambda x: x.scaleAll(1 / 1.5)),
     pygame.K_q: (lambda x: x.rotateAll('X', 0.05)),
     pygame.K_w: (lambda x: x.rotateAll('X', -0.05)),
     pygame.K_a: (lambda x: x.rotateAll('Y', -0.05)),
@@ -41,16 +41,20 @@ class ProjectionViewer:
         # if you want to use this module.
         self.font_size = 24
         self.my_font = pygame.font.SysFont('Comic Sans MS', self.font_size)
-        self.texts = [self.my_font.render(x,False, (200, 0, 0)) for x in ['l k move forward backward','q w rotate X-axis','a s rotate Y-axis','z x rotate Z-axis']]
-
+        self.texts = [self.my_font.render(x, False, (200, 0, 0)) for x in
+                      ['l k move forward backward', 'q w rotate X-axis', 'a s rotate Y-axis', 'z x rotate Z-axis']]
 
         self.background = (10, 10, 50)
         self.d = 500
         self.wireframes = {}
         self.displayNodes = True
         self.displayEdges = True
+        self.displayWalls = True
         self.nodeColour = (255, 255, 255)
         self.edgeColour = (200, 200, 200)
+        self.wallColours = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255),
+                           (255, 0, 255), (0, 128, 128), (128, 0, 128)]
+
         self.nodeRadius = 4
         self.zoom = 1
 
@@ -70,11 +74,12 @@ class ProjectionViewer:
                 elif event.type == pygame.KEYDOWN:
                     if event.key in key_to_function:
                         key_to_function[event.key](self)
+
                         # print(self.wireframes['cube'].nodes)
 
             self.display()
-            for i,text in enumerate(self.texts):
-                self.screen.blit(text, (0, self.font_size *i))
+            for i, text in enumerate(self.texts):
+                self.screen.blit(text, (0, self.font_size * i))
 
             pygame.display.flip()
 
@@ -82,24 +87,33 @@ class ProjectionViewer:
         """ Draw the wireframes on the screen. """
 
         self.screen.fill(self.background)
-        self.displayEdges = True
+        self.displayEdges = False
         for wireframe in self.wireframes.values():
-
             if self.displayNodes:
-                for i,node in enumerate(wireframe.nodes):
+                for i, node in enumerate(wireframe.nodes):
                     # pygame.draw.circle(self.screen, self.nodeColour, (int(node[0]), int(node[1])), self.nodeRadius, 0)
                     if node[2] > 0:
-                        point_x = (int(node[0] * self.d) / (node[2])*self.zoom +self.width/2)
-                        point_y = (int(node[1] * self.d) / (node[2] )*self.zoom + self.height/2)
-                        wireframe.nodes_2d_throw[i] = (point_x,point_y)
-                        pygame.draw.circle(self.screen, self.nodeColour, (point_x,point_y)  , self.nodeRadius, 0)
+                        point_x = (int(node[0] * self.d) / (node[2]) * self.zoom + self.width / 2)
+                        point_y = (int(node[1] * self.d) / (node[2]) * self.zoom + self.height / 2)
+                        wireframe.nodes_2d_throw[i] = (point_x, point_y)
+                        # pygame.draw.circle(self.screen, self.nodeColour, (point_x, point_y), self.nodeRadius, 0)
 
-                # matrix = wf.translationMatrix(*[self.width/2,self.height/2,0])
-                # wireframe.transform_throw(matrix)
                 if self.displayEdges:
                     for n1, n2 in wireframe.edges:
-                        pygame.draw.aaline(self.screen, self.edgeColour, wireframe.nodes_2d_throw[n1][:2], wireframe.nodes_2d_throw[n2][:2],
-                                           1)
+                        pygame.draw.aaline(self.screen, self.edgeColour, wireframe.nodes_2d_throw[n1][:2],
+                                           wireframe.nodes_2d_throw[n2][:2], 1)
+
+                        self.screen.blit(self.my_font.render(str(n1), False, (200, 0, 0)),
+                                         (wireframe.nodes_2d_throw[n1][:2]))
+                        self.screen.blit(self.my_font.render(str(n2), False, (200, 0, 0)),
+                                         (wireframe.nodes_2d_throw[n2][:2]))
+                if self.displayWalls:
+                    for id_color, wall in enumerate(wireframe.walls):
+                        pygame.draw.polygon(self.screen, wireframe.walls_colors[id_color],
+                                            wireframe.nodes_2d_throw[wall,:2])
+
+                        # pygame.draw.polygon(self.screen, self.wallColour,
+                        #                     [wireframe.nodes_2d_throw[i][:2] for i in wall])
 
     def translateAll(self, vector):
         matrix = wf.translationMatrix(*vector)
@@ -111,7 +125,7 @@ class ProjectionViewer:
         if scale > 1:
             self.zoom *= scale
         else:
-            if self.zoom !=1:
+            if self.zoom != 1:
                 self.zoom *= scale
 
     def rotateAll(self, axis, theta):
@@ -122,28 +136,30 @@ class ProjectionViewer:
             getattr(wireframe, rotateFunction)(theta)
             # self.translateAll([self.width/2,self.height/2,0])
 
+
 if __name__ == '__main__':
     height = 1920 / 2
     width = 1080 / 2
 
-    pv = ProjectionViewer(height, width )
-    for i  in range(1,6):
-        cube = wf.Wireframe()
+    pv = ProjectionViewer(height, width)
+    cube = wf.Wireframe()
+    cube_nodes = [(x, y, z) for x in (50, 250) for y in (50, 250) for z in (50, 250)]
 
-        modulo_i = i%2
-        if i == 5:
-            cube_nodes = [(x-200, y, z + 300 * 3) for x in (50, 250) for y in (50, 250) for z in (50, 250)]
-        elif i %2 == 0:
-            cube_nodes = [(x, y, z + 300 * i/2) for x in (50, 250) for y in (50, 250) for z in (50, 250)]
-        else:
-            cube_nodes = [(x-400, y, z + 300 * math.ceil(i/2)) for x in (50, 250) for y in (50, 250) for z in (50, 250)]
 
-        cube.addNodes(np.array(cube_nodes))
+    cube.addNodes(np.array(cube_nodes))
 
-        cube.addEdges(
-            [(n, n + 4) for n in range(0, 4)] + [(n, n + 1) for n in range(0, 8, 2)] + [(n, n + 2) for n in (0, 1, 4, 5)])
+    cube.addEdges(
+        [(n, n + 4) for n in range(0, 4)] + [(n, n + 1) for n in range(0, 8, 2)] + [(n, n + 2) for n in (0, 1, 4, 5)])
 
-        pv.addWireframe(f'cube_{i}', cube)
+    cube.add_walls(
+        [[0, 4, 6, 2],
+         [1, 5, 7, 3],
+         [7, 3, 2, 6],
+         [0, 1, 5, 4],
+         [1, 0, 2, 3],
+         [7, 6, 4, 5]], colors=  [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255)]
+    )
 
+    pv.addWireframe(f'cube', cube)
 
     pv.run()
--- camera-3D/wireframe.py
+++ camera-3D/wireframe.py
@@ -6,8 +6,10 @@ class Wireframe:
         self.nodes = np.zeros((0, 4))
         self.nodes_2d_throw = np.zeros((0, 2))
         self.edges = []
-
-
+        self.walls = np.zeros((0, 4))
+        self.walls_z = np.zeros((0, 4))
+        self.walls_x_y_norm =  np.zeros((0, 4))
+        self.walls_colors = np.zeros((0, 3))
 
     def addNodes(self, node_array):
         ones_column = np.ones((len(node_array), 1))
@@ -18,57 +20,95 @@ class Wireframe:
     def addEdges(self, edgeList):
         self.edges += edgeList
 
+    def update_wall_z_and_x_Y_norm(self):
+        self.walls_z = self.nodes[self.walls, 2]
+        self.walls_x_y_norm = np.linalg.norm(self.nodes[self.walls, :3], axis=2)
+
+    def add_walls(self, wallList, colors):
+        self.walls = np.array(wallList)
+        self.update_wall_z_and_x_Y_norm()
+        self.walls_colors = np.array(colors)
+
+    def sort_by_z_walls(self):
+
+        self.walls_z = np.sort(self.walls_z, axis=1,kind='stable')
+        self.walls_x_y_norm = np.sort(self.walls_x_y_norm, axis=1,kind='stable')
+
+        to_sort = np.hstack((self.walls_x_y_norm, self.walls_z))
+        sorted_index = np.lexsort([to_sort[:, -i] for i in reversed(range(1,9))] )[::-1]
+
+        self.walls = self.walls[sorted_index]
+        self.walls_z = self.walls_z[sorted_index]
+        self.walls_x_y_norm = self.walls_x_y_norm[sorted_index]
+        self.walls_colors = self.walls_colors[sorted_index]
+        # print('po sortowaniu')
+        # print(sorted_index)
+        # print(self.walls)
+        # print(self.walls_z)
+        # print(to_sort[sorted_index])
+
     def outputNodes(self):
 
         for i in range(self.nodes.shape[0]):
-            x, y, z,_ = self.nodes[i]
+            x, y, z, _ = self.nodes[i]
             print("Node %d: (%.3f, %.3f, %.3f)" % (i, x, y, z))
-            
+
     def outputEdges(self):
         for i, (start, stop) in enumerate(self.edges):
             node1 = self.nodes[start]
             node2 = self.nodes[stop]
-            print( "Edge %d: (%.3f, %.3f, %.3f)" % (i, node1[0], node1[1], node1[2]))
-            print( "to (%.3f, %.3f, %.3f)" % (node2[0], node2[1], node2[2]))
+            print("Edge %d: (%.3f, %.3f, %.3f)" % (i, node1[0], node1[1], node1[2]))
+            print("to (%.3f, %.3f, %.3f)" % (node2[0], node2[1], node2[2]))
 
     def findCentre(self):
         meanX, meanY, meanZ, _ = self.nodes.mean(axis=0)
         return (meanX, meanY, meanZ)
 
-
     def transform(self, matrix):
         self.nodes = np.dot(self.nodes, matrix)
+        self.update_wall_z_and_x_Y_norm()
+        self.sort_by_z_walls()
 
     def transform_throw(self, matrix):
         self.nodes_2d_throw = np.dot(self.nodes_2d_throw, matrix)
 
-    def scale(self,scale):
+    # def scale(self,scale):
+    #
+    #     matrix = scaleMatrix(scale)
+    #     self.transform(matrix)
 
-        matrix = scaleMatrix(scale)
-        self.transform(matrix)
-
-
-    def rotateX(self,theta=1):
+    def rotateX(self, theta=1):
         self.transform(rotateXMatrix(theta))
 
-    def rotateY(self,theta=1):
+    def rotateY(self, theta=1):
         self.transform(rotateYMatrix(theta))
 
-    def rotateZ(self,theta=1):
+    def rotateZ(self, theta=1):
         self.transform(rotateZMatrix(theta))
 
-# if __name__ == "__main__":
-#     cube = Wireframe()
-#     cube_nodes = [(x,y,z) for x in (0,1) for y in (0,1) for z in (0,1)]
-#     cube.addNodes(np.array(cube_nodes))
-#
-#     cube.addEdges([(n,n+4) for n in range(0,4)])
-#     cube.addEdges([(n,n+1) for n in range(0,8,2)])
-#     cube.addEdges([(n,n+2) for n in (0,1,4,5)])
-#
-#     cube.findCentre()
-#     cube.outputNodes()
-#     cube.outputEdges()
+
+if __name__ == "__main__":
+    cube = Wireframe()
+    cube_nodes = [(x, y, z) for x in (50, 250) for y in (50, 250) for z in (50, 250)]
+
+    cube.addNodes(np.array(cube_nodes))
+
+    cube.addEdges(
+        [(n, n + 4) for n in range(0, 4)] + [(n, n + 1) for n in range(0, 8, 2)] + [(n, n + 2) for n in (0, 1, 4, 5)])
+
+    cube.add_walls(
+        [[0, 4, 6, 2],
+         [1, 5, 7, 3],
+         [7, 3, 2, 6],
+         [0, 1, 5, 4],
+         [1, 0, 2, 3],
+         [7, 6, 4, 5]], colors= np.zeros((6,3))
+    )
+    cube.sort_by_z_walls()
+    cube.sort_by_z_walls()
+
+    # cube.outputNodes()
+    # cube.outputEdges()
 
 
 def translationMatrix(dx=0, dy=0, dz=0):
@@ -76,6 +116,7 @@ def translationMatrix(dx=0, dy=0, dz=0):
                      [0, 1, 0, 0],
                      [0, 0, 1, 0],
                      [dx, dy, dz, 1]])
+
 
 def rotateXMatrix(radians):
     c = np.cos(radians)
@@ -85,6 +126,7 @@ def rotateXMatrix(radians):
                      [0, -s, c, 0],
                      [0, 0, 0, 1]])
 
+
 def rotateYMatrix(radians):
     c = np.cos(radians)
     s = np.sin(radians)
@@ -92,6 +134,7 @@ def rotateYMatrix(radians):
                      [0, 1, 0, 0],
                      [-s, 0, c, 0],
                      [0, 0, 0, 1]])
+
 
 def rotateZMatrix(radians):
     c = np.cos(radians)
@@ -101,12 +144,11 @@ def rotateZMatrix(radians):
                      [0, 0, 1, 0],
                      [0, 0, 0, 1]])
 
-def scaleMatrix(z):
-    zoom = 1 /(1-z)
-    zoom_1 = -z/(1-z)
-    return np.array([[1, 0, 0, 0],
-                     [0, 1, 0, 0],
-                     [0, 0, zoom,1],
-                     [0, 0, zoom_1, 0]]
-                    )
-
+# def scaleMatrix(z):
+#     zoom = 1 /(1-z)
+#     zoom_1 = -z/(1-z)
+#     return np.array([[1, 0, 0, 0],
+#                      [0, 1, 0, 0],
+#                      [0, 0, zoom,1],
+#                      [0, 0, zoom_1, 0]]
+#                     )
